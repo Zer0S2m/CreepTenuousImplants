9779c4b0be145ced3c7059d53f2d42eea486c5b1
aleksandr <silkin.a@claramente.ru>
2023-06-11 21:26:22 +0300

    init

---
+++ src/main/java/com/zer0s2m/creeptenuous/implants/CreepTenuousImplantsApplication.java
@@ -0,0 +1,13 @@
+package com.zer0s2m.creeptenuous.implants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreepTenuousImplantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreepTenuousImplantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zer0s2m/creeptenuous/implants/CreepTenuousImplantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zer0s2m.creeptenuous.implants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreepTenuousImplantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
