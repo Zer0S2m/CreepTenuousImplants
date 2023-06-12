dca05f56ed797a628305422026a4ade81bae14b5
aleksandr <silkin.a@claramente.ru>
2023-06-12 10:25:26 +0300

    First task

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CreepTenuousImplants
+
+The project requires a deployed system [CreepTenuous](https://github.com/Zer0S2m/CreepTenuous).
+
+Before starting, specify the environment variables that are in the main system.
--- src/main/java/com/zer0s2m/creeptenuous/implants/CreepTenuousImplantsApplication.java
+++ src/main/java/com/zer0s2m/creeptenuous/implants/CreepTenuousImplantsApplication.java
@@ -2,7 +2,9 @@ package com.zer0s2m.creeptenuous.implants;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CreepTenuousImplantsApplication {
 
