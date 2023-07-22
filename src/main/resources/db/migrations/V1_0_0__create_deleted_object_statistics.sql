2c63acf392b7aff4f0c74f0f7aa5e74c48c2840b
Aleksandr <ZeroDay9898@gmail.com>
2023-07-22 23:24:15 +0300

    Basic structure for collecting statistics

---
+++ src/main/resources/db/migrations/V1_0_0__create_deleted_object_statistics.sql
@@ -0,0 +1,18 @@
+CREATE DOMAIN type_object_deleted
+    AS VARCHAR
+    CONSTRAINT type_object_deleted
+        CHECK (VALUE IN (
+                         'DIRECTORY',
+                         'FILE',
+                         'DIRECTORY_REDIS',
+                         'FILE_REDIS',
+                         'UNIDENTIFIED_TYPE'
+            ));
+
+CREATE TABLE IF NOT EXISTS "deleted_object_statistics"
+(
+    id          BIGSERIAL PRIMARY KEY NOT NULL,
+    user_login  VARCHAR(30)           NOT NULL DEFAULT 'NOT_LOGIN',
+    created_at  TIMESTAMP             NOT NULL DEFAULT NOW(),
+    type_object type_object_deleted   NOT NULL DEFAULT 'UNIDENTIFIED_TYPE'
+);
